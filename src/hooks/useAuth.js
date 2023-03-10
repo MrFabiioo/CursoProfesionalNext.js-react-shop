b5f16e37b0f696eaf6a2aefba02b0edb10c369b7
MrFabiioo <fabiolin57@gmail.com>
2023-03-09 20:12:01 -0500

    clase 12

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,27 +1,37 @@
-import React, {useState, useContext, createContext} from 'react';
-import Cookie from 'js-cookie'; //Nos ayuda asignar a nuestro navegador las cookies que esté recibiendo en el momento de la autenticación
-import axios from 'axios'; //Para el manejo de las peticiones como GET, PUT, POST, DELETE
+import React, { useState, useContext, createContext } from 'react';
+import Cookie from 'js-cookie';
+import axios from 'axios';
+import endPoints from '@services/api/';
 
-const AuthContext = createContext(); //Se crea un nuevo context gracias a la api de react
-//Se crea la encapsulación de nuestra aplicación
-export function ProviderAuth({children}){
-	const auth = useProvideAuth();
-	return <AuthContext.Provider value ={auth}>{children}</AuthContext.Provider>;
-}
-//Permite exponer cierta información que se estará requiriendo
-export const useAuth = () => {
-	return useContext(AuthContext);
-};
-//Captar la información del usuario
-function useProvideAuth() {
-	const [user, setUser] = useState(null);
+const AuthContext = createContext();
 
-	const signIn = async (email, password) => {
-	setUser('login');
-	};
+export function ProviderAuth({ children }) {
+    const auth = useProvideAuth();
+    return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+    }
 
-	return{
-		user,
-		signIn,
-	};
+    export const useAuth = () => {
+    return useContext(AuthContext);
+    };
+
+    function useProvideAuth() {
+    const [user, setUser] = useState(null);
+
+    const signIn = async (email, password) => {
+        const options = {
+        headers: {
+            accept: '*/*',
+            'Content-Type': 'application/json',
+        },
+        };
+        const {
+            data: { access_token },
+            } = await axios.post(endPoints.auth.login, { email, password }, options)
+        console.log(access_token);
+    };
+
+    return {
+        user,
+        signIn,
+    };
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,17 @@
 import { ProviderAuth } from '@hooks/useAuth';
-import MainLayout from '@layaout/MainLayout';
+import MainLayout from '@layout/MainLayout';
 import '@styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-return (
-<>
-<ProviderAuth>
-<MainLayout>
-<Component {...pageProps} />
-</MainLayout>
-</ProviderAuth>
-</>
-);
+  return (
+    <>
+      <ProviderAuth>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </ProviderAuth>
+    </>
+  );
 }
 
 export default MyApp;
